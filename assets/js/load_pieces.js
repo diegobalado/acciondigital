4dfe3ae777d2acdd9c9a8c7380d060438882bd45
Diego <jdiegomdq@gmail.com>
2017-04-02 01:48:03 -0300

    clean refactoring - header logo

---
+++ assets/js/load_pieces.js
@@ -0,0 +1,6 @@
+// $('#header').load('./assets/includes/header.htm');
+$('#gallery-ad').load('./assets/includes/gallery-ad.htm');
+$('#contact').load('./assets/includes/section-contact.htm');
+$('#footer').load('./assets/includes/footer.htm');
+$('#nav').load('./assets/includes/nav.htm');
+$('#nav-header').load('./assets/includes/nav.htm');
---
+++ assets/js/scripts.js
@@ -0,0 +1,37 @@
+/*HANDLEBARS*/
+$('body').append('<script id="pictures-template" type="text/x-handlebars-template"></scr'+'ipt>');
+$('#pictures-template').load('dataDetailsTemplate.handlebars');
+//wait for page to load
+$(document).ready(function(){
+  // Extract the text from the template .html() is the jquery helper method for that
+  var raw_template = $('#pictures-template').html();
+  // Compile that into an handlebars template
+  var template = Handlebars.compile(raw_template);
+  // Retrieve the placeHolder where the Posts will be displayed 
+  var placeHolder = $("#gallery-wrapper");
+  // Fetch all Blog Posts data from server in JSON
+  $.get("pictures.json", function(data,status,xhr){
+  	var html = template(JSON.parse(data));
+    // Render the posts into the page
+    placeHolder.append(html);
+  });
+});
+
+/*HEADER COLAPSABLE*/
+$(function(){
+  var shrinkHeader = 150;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+    if ( scroll >= shrinkHeader ) {
+      $('#header').addClass('shrink');
+      $('#nav-header').addClass('fixed');
+    }
+    else {
+      $('#header').removeClass('shrink');
+      $('#nav-header').removeClass('fixed');
+    }
+  });
+  function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
+});
