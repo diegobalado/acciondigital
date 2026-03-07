5e51c265762585e985bd31c01f3a552ff9d88e3b
Diego Balado <jdiegomdq@gmail.com>
2026-03-07 09:22:48 -0300

    refactor(auth): extract shared ads multiselect module

--- auth/create_home.php
+++ auth/create_home.php
@@ -110,20 +110,7 @@
 	<script type="text/javascript" src="/assets/js/modules/ui.js"></script>
 	<script type="text/javascript" src="/assets/js/scripts_auth.js"></script>
 	<script type="text/javascript" src="/assets/js/jquery.multi-select.js"></script>
-	<script type="text/javascript">
-		const adsFinal = [];
-		$('#ads').multiSelect({
-			keepOrder: true,
-			afterSelect: function (values) {
-				adsFinal.push(values[0]);
-				$('#adsOrdered').val(JSON.stringify(adsFinal));
-			},
-			afterDeselect: function (values) {
-				adsFinal.splice(adsFinal.indexOf(values.toString()), 1);
-				$('#adsOrdered').val(JSON.stringify(adsFinal));
-			}
-		});
-	</script>
+	<script type="text/javascript" src="/assets/js/modules/auth-ads-multiselect.js"></script>
 
 </body>
 </html>
